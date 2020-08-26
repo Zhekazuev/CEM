f4c66fccfb231663a12d7acaaddfee7f756fefad
Evgeny Zuev <zueven@mts.by>
2020-08-26 20:34:08 +0300

    Change info-text

--- fullflow.py
+++ fullflow.py
@@ -41,20 +41,22 @@ def logic(df):
                        (df_ipstarts_1965['BYTES UPLINK'].sum())).sort_values(ascending=False).sum().div(1024 ** 2)
     traffic_ip_1491 = ((df_ipstarts_1491['BYTES DOWNLINK'].sum()) +
                        (df_ipstarts_1491['BYTES UPLINK'].sum())).sort_values(ascending=False).sum().div(1024 ** 2)
-    summary_telegram_traffic = traffic_ip_451 + traffic_ip_1965 + traffic_ip_1491
-    print(f"Total Telegram traffic for return to subscriber: {summary_telegram_traffic} MB")
+    total_telegram_traffic = traffic_ip_451 + traffic_ip_1965 + traffic_ip_1491
+    print(f"Total Telegram traffic for return to subscriber: {total_telegram_traffic} MB")
 
     # instagram
     instagram_addresses = addresses["instagram"]
     df_1 = df[df['SN CHARGING ACTION'] != 'Social-net']
     df_2 = df_1[df_1['SERVER IP ADDRESS'].isin(instagram_addresses)]
-    summary_instagram_traffic = ((df_2.groupby('SN CHARGING ACTION')['BYTES DOWNLINK']).sum() +
+    total_instagram_traffic = ((df_2.groupby('SN CHARGING ACTION')['BYTES DOWNLINK']).sum() +
                                  (df_2.groupby('SN CHARGING ACTION')['BYTES UPLINK']).sum()).div(1024 ** 2)
-    print(f"Total Instagram traffic for return to subscriber: {summary_instagram_traffic} MB")
+    print(f"Total Instagram traffic for return to subscriber: {total_instagram_traffic} MB")
 
     # youtube
     # summary_youtube_traffic =
-    return f"Total instagram traffic for return to subscriber: {summary_telegram_traffic + summary_instagram_traffic} MB"
+
+    total_traffic = total_telegram_traffic + total_instagram_traffic
+    return f"Total instagram traffic for return to subscriber: {total_traffic} MB"
 
 
 def database():
