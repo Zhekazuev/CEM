83a35901b183e621f3d049de41f43ddd5f9cb2b9
Evgeny Zuev <zueven@mts.by>
2020-08-26 20:16:39 +0300

    Rename and combined Rename flow.py file to fullflow.py and combined two methods of Instagram and Telegram

--- fullflow.py
+++ fullflow.py
@@ -1,22 +1,21 @@
+from clickhouse_driver import connect
 import pandas as pd
 import config
 import sys
 
 
-def file(path):
-    df = pd.read_csv(path, sep=";")
-
+def logic(df):
     # protocol statistics
     protocol_statisctics = ((df.groupby('P2P PROTOCOL')['BYTES DOWNLINK'].sum() / (1024 ** 2) +
                              df.groupby('P2P PROTOCOL')['BYTES UPLINK'].sum() / (1024 ** 2))
                             .sort_values(ascending=False))
-    print("Summury protocols statistics:", protocol_statisctics)
+    print("Total statistics by Applications(Protocols):", protocol_statisctics)
 
     # charging action statistics
     actions_statisctics = ((df.groupby('SN CHARGING ACTION')['BYTES DOWNLINK'].sum() / (1024 ** 2) +
                             df.groupby('SN CHARGING ACTION')['BYTES UPLINK'].sum() / (1024 ** 2))
                            .sort_values(ascending=False))
-    print("Summury charging actions statistics:", actions_statisctics)
+    print("Total statistics by Rules:", actions_statisctics)
 
     # get all unique addresses
     addresses = df.groupby('P2P PROTOCOL')['SERVER IP ADDRESS'].unique()
@@ -36,7 +35,7 @@ def file(path):
     traffic_ip_1491 = ((df_ipstarts_1491['BYTES DOWNLINK'].sum()) +
                        (df_ipstarts_1491['BYTES UPLINK'].sum())).sort_values(ascending=False).sum()/(1024**2)
     summary_telegram_traffic = traffic_ip_451 + traffic_ip_1965 + traffic_ip_1491
-    print(f"Summary telegram traffic for return: {summary_telegram_traffic} MB")
+    print(f"Total Telegram traffic for return to subscriber: {summary_telegram_traffic} MB")
 
     # instagram
     instagram_addresses = addresses["instagram"]
@@ -44,24 +43,45 @@ def file(path):
     df_2 = df_1[df_1['SERVER IP ADDRESS'].isin(instagram_addresses)]
     summary_instagram_traffic = ((df_2.groupby('SN CHARGING ACTION')['BYTES DOWNLINK']).sum() +
                                  (df_2.groupby('SN CHARGING ACTION')['BYTES UPLINK']).sum()) / (1024 ** 2)
-    print(f"Summary instagram traffic for return: {summary_instagram_traffic} MB")
+    print(f"Total Instagram traffic for return to subscriber: {summary_instagram_traffic} MB")
 
     # youtube
-
-    return f"Summury traffic for return: {summary_telegram_traffic + summary_instagram_traffic} MB"
+    # summary_youtube_traffic =
+    return f"Total instagram traffic for return to subscriber: {summary_telegram_traffic + summary_instagram_traffic} MB"
 
 
 def database():
-    pass
+    print("Phone number format: 375291234567")
+    # check number format
+    number = input("Input MSISDN here:")
+
+    print("Data start format: 21-08-2020")
+    # check data start
+    data_start = input("Input Data start here:")
+
+    print("Data finish format: 21-08-2020")
+    data_start = input("Input Data finish here:")
+
+    con = connect(host=config.host,
+                  port=config.port,
+                  user=config.login,
+                  password=config.password)
+
+    query = ""
+    df = pd.read_sql_query(query, con)
+
+    return df
 
 
 def main():
     try:
         path = sys.argv[1]
-        print(file(path))
+        df = pd.read_csv(path, sep=";")
     except IndexError:
-        print(database())
+        df = database()
+
+    return logic(df)
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
