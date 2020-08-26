fa99602a5f8588ccb55b0eecf3256286fbe59665
Evgeny Zuev <zueven@mts.by>
2020-08-26 20:33:53 +0300

    Change info-text and methods' names

--- telegram.py
+++ telegram.py
@@ -1,50 +1,61 @@
+from clickhouse_driver import connect
 import pandas as pd
 import config
 import sys
 
 
-def file(path):
-    df = pd.read_csv(path, sep=";")
-
-    protocol_statisctics = ((df.groupby('P2P PROTOCOL')['BYTES DOWNLINK'].sum() / (1024 ** 2) +
-                             df.groupby('P2P PROTOCOL')['BYTES UPLINK'].sum() / (1024 ** 2))
-                            .sort_values(ascending=False))
-    print("Summury protocols statistics:", protocol_statisctics)
-
-    actions_statisctics = ((df.groupby('SN CHARGING ACTION')['BYTES DOWNLINK'].sum() / (1024 ** 2) +
-                            df.groupby('SN CHARGING ACTION')['BYTES UPLINK'].sum() / (1024 ** 2))
-                           .sort_values(ascending=False))
-    print("Summury charging actions statistics:", actions_statisctics)
-
-    df_no_messengers = df[df['SN CHARGING ACTION'] != 'Messengers']
-    df_ipstarts_451 = (df_no_messengers[df_no_messengers['SERVER IP ADDRESS'].str.match('^45.1*') == True]
+def logic(df):
+    # telegram
+    no_messengers = df[df['SN CHARGING ACTION'] != 'Messengers']
+    df_ipstarts_451 = (no_messengers[no_messengers['SERVER IP ADDRESS'].str.match('^45.1*') == True]
                        .groupby('SN CHARGING ACTION'))
-    df_ipstarts_1965 = (df_no_messengers[df_no_messengers['SERVER IP ADDRESS'].str.match('^196.5*') == True]
+    df_ipstarts_1965 = (no_messengers[no_messengers['SERVER IP ADDRESS'].str.match('^196.5*') == True]
                         .groupby('SN CHARGING ACTION'))
-    df_ipstarts_1491 = (df_no_messengers[df_no_messengers['SERVER IP ADDRESS'].str.match('^149.1*') == True]
+    df_ipstarts_1491 = (no_messengers[no_messengers['SERVER IP ADDRESS'].str.match('^149.1*') == True]
                         .groupby('SN CHARGING ACTION'))
-
     traffic_ip_451 = ((df_ipstarts_451['BYTES DOWNLINK'].sum()) +
-                      (df_ipstarts_451['BYTES UPLINK'].sum())).sort_values(ascending=False).sum()/(1024**2)
+                      (df_ipstarts_451['BYTES UPLINK'].sum())).sort_values(ascending=False).sum().div(1024 ** 2)
     traffic_ip_1965 = ((df_ipstarts_1965['BYTES DOWNLINK'].sum()) +
-                       (df_ipstarts_1965['BYTES UPLINK'].sum())).sort_values(ascending=False).sum()/(1024**2)
+                       (df_ipstarts_1965['BYTES UPLINK'].sum())).sort_values(ascending=False).sum().div(1024 ** 2)
     traffic_ip_1491 = ((df_ipstarts_1491['BYTES DOWNLINK'].sum()) +
-                       (df_ipstarts_1491['BYTES UPLINK'].sum())).sort_values(ascending=False).sum()/(1024**2)
+                       (df_ipstarts_1491['BYTES UPLINK'].sum())).sort_values(ascending=False).sum().div(1024 ** 2)
+    total_telegram_traffic = traffic_ip_451 + traffic_ip_1965 + traffic_ip_1491
 
-    return f"Summury traffic for return: {traffic_ip_451 + traffic_ip_1965 + traffic_ip_1491} MB"
+    return f"Total instagram traffic for return to subscriber: {total_telegram_traffic} MB"
 
 
 def database():
-    pass
+    print("Phone number format: 375291234567")
+    # check number format
+    number = input("Input MSISDN here:")
+
+    print("Data start format: 21-08-2020")
+    # check data start
+    data_start = input("Input Data start here:")
+
+    print("Data finish format: 21-08-2020")
+    data_start = input("Input Data finish here:")
+
+    con = connect(host=config.host,
+                  port=config.port,
+                  user=config.login,
+                  password=config.password)
+
+    query = ""
+    df = pd.read_sql_query(query, con)
+
+    return df
 
 
 def main():
     try:
         path = sys.argv[1]
-        print(file(path))
+        df = pd.read_csv(path, sep=";")
     except IndexError:
-        print(database())
+        df = database()
+
+    return logic(df)
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
